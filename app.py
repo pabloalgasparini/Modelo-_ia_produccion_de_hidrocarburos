9662378742ae0edfc411b955886505a84cc8c04c
pabloalgasparini <pabloalgasparini@gmail.com>
2023-12-06 14:07:54 -0300

    cors incluido

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from  fastapi import FastAPI
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.ia import ia
 
 app = FastAPI(
-    title = "REST API con FastAPI ",
-    description = "Modelo entrenado para pozos de hidrocarburos",
-    version = "0.0.1"
+    title="REST API con FastAPI",
+    description="Modelo entrenado para pozos de hidrocarburos",
+    version="0.0.1",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  
+    allow_credentials=True,
+    allow_methods=["GET"],
+    allow_headers=["*"],
 )
 
 app.include_router(ia)
